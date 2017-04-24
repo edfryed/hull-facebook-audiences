02674dc7269ce82bb7b2cf1734a24924ba1ca625
mkawalek <michalkawalekdevkit@icloud.com>
2017-04-24 14:26:57 +0200

    fixes after tests, app.json updates

--- server/server.js
+++ server/server.js
@@ -5,14 +5,6 @@ import { notifHandler, batchHandler } from "hull/lib/utils";
 import FacebookAudience from "./facebook-audience";
 import adminHandler from "./handlers/admin";
 
-function onError(err, req, res, next) { // eslint-disable-line no-unused-vars
-  // The error id is attached to `res.sentry` to be returned
-  // and optionally displayed to the user for support.
-  console.error(err);
-  res.statusCode = 500;
-  res.end(`${res.sentry}\n`);
-}
-
 module.exports = function Server(options) {
   const { connector, app, facebookAppId, facebookAppSecret, sentryDSN } = options;
 
@@ -37,10 +29,9 @@ module.exports = function Server(options) {
   app.use("/batch", (req, res, next) => {
     req.hull.query = req.query;
     next();
-  }, batchHandler(({ client, ship, helpers, segments, metric, query }, messages = []) => {
+  }, batchHandler(({ client, ship, helpers, segments, metric, query }, users = []) => {
     const { audience } = query;
     const fb = new FacebookAudience(ship, client, helpers, segments, metric);
-    const users = messages.map(m => m.message.user);
     if (audience && users) {
       fb.addUsersToAudience(audience, users);
     }
@@ -53,8 +44,6 @@ module.exports = function Server(options) {
   if (sentryDSN) {
     app.use(raven.middleware.express.errorHandler(sentryDSN));
   }
-
-  app.use(onError);
 
   return app;
 }
