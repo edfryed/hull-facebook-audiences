af985e52822080be8576573c119911a3123b3c28
Michal Raczka <michal@raczka.me>
2017-07-21 13:13:55 +0200

    Upgrade the structure and prepare for new 0.12

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 import express from "express";
 import Promise from "bluebird";
 
-import { notifHandler, batchHandler } from "hull/lib/utils";
+import { notifHandler } from "hull/lib/utils";
 
 import FacebookAudience from "./lib/facebook-audience";
 import adminHandler from "./handlers/admin";
@@ -24,16 +24,18 @@ export default function server(app: express, dependencies: Object): express {
   /**
    * Handles batches. Only those which are sent with additional audience param - so ones requested from the ship.
    */
-  app.use("/batch", batchHandler(({ client, ship, helpers, segments, metric, options }, messages = []) => {
-    const { audience } = options;
-    const users = messages.map(m => m.user);
-    const fb = new FacebookAudience(ship, client, helpers, segments, metric);
-    if (audience && users) {
-      return fb.addUsersToAudience(audience, users);
+  app.use("/batch", notifHandler({
+    handlers: {
+      "user:update": ({ client, ship, helpers, segments, metric, options }, messages = []) => {
+        const { audience } = options;
+        const users = messages.map(m => m.user);
+        const fb = new FacebookAudience(ship, client, helpers, segments, metric);
+        if (audience && users) {
+          return fb.addUsersToAudience(audience, users);
+        }
+        return Promise.resolve();
+      }
     }
-    return Promise.resolve();
-  }, {
-    groupTraits: false
   }));
 
   app.use("/admin", adminHandler({ facebookAppSecret, facebookAppId }));
